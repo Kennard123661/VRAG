64bfb686d66179e9ad301d2a0a9b4d326f14ff54
Kennard Ng <kennard_ng@outlook.com>
2021-03-31 21:45:38 +0800

    hotfix

--- vrag/directory.py
+++ vrag/directory.py
@@ -1,6 +1,5 @@
 import os
 
-PROJECT_DIR = os.environ['VRAG_PATH_PROJECT_DIR']
 CCWEB_DIR = os.environ['VRAG_PATH_CCWEB_DIR']
 EVVE_DIR = os.environ['VRAG_PATH_EVVE_DIR']
 FIVR5K_DIR = os.environ['VRAG_PATH_FIVR5K_DIR']
@@ -8,8 +7,8 @@ FIVR200K_DIR = os.environ['VRAG_PATH_FIVR200K_DIR']
 VCDB_DIR = os.environ['VRAG_PATH_VCDB_DIR']
 VCDB_DISTRACTOR_DIR = os.environ['VRAG_PATH_VCDB_DISTRACTOR_DIR']
 TRAIN_DATA_DIR = os.environ['VRAG_PATH_TRAIN_DATA_DIR']
+CHECKPOINT_DIR = os.environ['VRAG_PATH_CHECKPOINT_DIR']
 
-CHECKPOINT_DIR = os.path.join(PROJECT_DIR, 'checkpoints')
-CONFIG_DIR = os.path.join(PROJECT_DIR, 'configs')
-LOG_DIR = os.path.join(PROJECT_DIR, 'logs')
-RESULT_DIR = os.path.join(PROJECT_DIR, 'results')
+CONFIG_DIR = os.environ['VRAG_PATH_CONFIG_DIR']
+LOG_DIR = os.environ['VRAG_PATH_LOG_DIR']
+RESULT_DIR = os.environ['VRAG_PATH_RESULT_DIR']
