b3f2aa067ea22f8e7726c3f11c9ace13e0de920d
Kennard Ng <kennard_ng@outlook.com>
2021-03-31 21:40:09 +0800

    fix

--- vrag/directory.py
+++ vrag/directory.py
@@ -1,45 +1,13 @@
 import os
-import platform
 
-node = platform.node()
-if node == 'paul':
-    DATASET_DIR = '/mnt/BigData/video-retrieval-datasets'
-    DATASET_DIR1 = '/mnt/HGST6/video-retrieval-datasets'
-    PROJECT_DIR = '/mnt/BigData/projects/VRAG'
-
-    CCWEB_DIR = os.path.join(DATASET_DIR, 'ccweb-video')
-    EVVE_DIR = os.path.join(DATASET_DIR1, 'evve')
-    FIVR5K_DIR = os.path.join(DATASET_DIR1, 'fivr5k')
-    FIVR200K_DIR = os.path.join(DATASET_DIR, 'fivr200k')
-    VCDB_DIR = os.path.join(DATASET_DIR, 'vcdb')
-    VCDB_DISTRACTOR_DIR = '/mnt/WD6/vcdb-distractors'
-    TRAIN_DATA_DIR = os.path.join(DATASET_DIR1, 'video-retrieval-train')
-elif node == 'deep-one':
-    DATASET_DIR = '/mnt/BigData/video-retrieval-datasets'
-    DATASET_DIR1 = '/mnt/Data/kennard/video-retrieval-datasets'
-    PROJECT_DIR = '/mnt/BigData/code/gcn'
-
-    CCWEB_DIR = os.path.join(DATASET_DIR, 'ccweb-video')
-    EVVE_DIR = os.path.join(DATASET_DIR1, 'evve')
-    FIVR5K_DIR = os.path.join(DATASET_DIR1, 'fivr5k')
-    FIVR200K_DIR = os.path.join(DATASET_DIR, 'fivr200k')
-    VCDB_DIR = os.path.join(DATASET_DIR, 'vcdb')
-    VCDB_DISTRACTOR_DIR = os.path.join(DATASET_DIR, 'vcdb-distractors')
-    TRAIN_DATA_DIR = os.path.join(DATASET_DIR, 'train-data')
-elif node == 'kennardng-desktop':
-    DATASET_DIR = '/mnt/BigData/project-storage/VRAG/datasets'
-    DATASET_DIR1 = '/mnt/Data/project-storage/VRAG/datasets'
-    PROJECT_DIR = '/home/kennardng/projects/VRAG'
-
-    CCWEB_DIR = os.path.join(DATASET_DIR, 'ccweb-video')
-    EVVE_DIR = os.path.join(DATASET_DIR1, 'evve')
-    FIVR5K_DIR = os.path.join(DATASET_DIR1, 'fivr5k')
-    FIVR200K_DIR = os.path.join(DATASET_DIR, 'fivr200k')
-    VCDB_DIR = os.path.join(DATASET_DIR, 'vcdb')
-    VCDB_DISTRACTOR_DIR = os.path.join(DATASET_DIR, 'vcdb-distractors')
-    TRAIN_DATA_DIR = os.path.join(DATASET_DIR, 'train-data')
-else:
-    raise ValueError('ERR: no such node hostname')
+PROJECT_DIR = os.environ['VRAG_PATH_PROJECT_DIR']
+CCWEB_DIR = os.environ['VRAG_PATH_CCWEB_DIR']
+EVVE_DIR = os.environ['VRAG_PATH_EVVE_DIR']
+FIVR5K_DIR = os.environ['VRAG_PATH_FIVR5K_DIR']
+FIVR200K_DIR = os.environ['VRAG_PATH_FIVR200K_DIR']
+VCDB_DIR = os.environ['VRAG_PATH_VCDB_DIR']
+VCDB_DISTRACTOR_DIR = os.environ['VRAG_PATH_VCDB_DISTRACTOR_DIR']
+TRAIN_DATA_DIR = os.environ['VRAG_PATH_TRAIN_DATA_DIR']
 
 CHECKPOINT_DIR = os.path.join(PROJECT_DIR, 'checkpoints')
 CONFIG_DIR = os.path.join(PROJECT_DIR, 'configs')
